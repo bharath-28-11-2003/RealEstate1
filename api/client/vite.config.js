d1290a4f6c08d3c64a97e0c331c953fb61e3714b
bharath-28-11-2003 <130736965+bharath-28-11-2003@users.noreply.github.com>
2024-06-09 11:42:42 +0530

    vite-plug

--- api/client/vite.config.js
+++ api/client/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Inspect from 'vite-plugin-inspect'
+import react from 'vite-plugin-react'
+
 
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Inspect()],
+  plugins: [react()],
   
   
     
