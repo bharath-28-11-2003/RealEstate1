935fedcbf7bcd4cfe746cc74df24a152d8bf7a6a
bharath-28-11-2003 <130736965+bharath-28-11-2003@users.noreply.github.com>
2024-06-09 11:20:15 +0530

    viteerror

--- api/client/vite.config.js
+++ api/client/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-import react from "save-dev"
+import react from '@vitejs/plugin-react'
+
 
 
 // https://vitejs.dev/config/
